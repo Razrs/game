e7e2f0c36b3ed214b7b132d14853b84bec17ed25
Benjamin Cheng <lolzballs.mc@gmail.com>
2014-12-06 10:18:57 -0500

    Rotating square because yeah

--- scripts/game.js
+++ scripts/game.js
@@ -7,14 +7,31 @@ function load() {
 	// Add the element to body
 	document.body.appendChild(renderer.view);
 
-	// Gets an anim frame, equaliant to render method
+	initTextures();
+
+	var square = newSquare();
+	stage.addChild(square);
+
+	// Gets an anim frame, equalviant to render method
 	requestAnimFrame(animate);
 
 	function animate() {
 		// Recursion to keep rendering
 		requestAnimFrame(animate);
 
+		square.rotation += 0.1;
+
 		// Render to the stage
 		renderer.render(stage);
 	}
+}
+
+function newSquare() {
+	var square = new PIXI.Sprite(textureSquare);
+	square.anchor.x = 0;
+	square.anchor.y = 0;
+	square.position.x = 0;
+	square.position.y = 0;
+
+	return square;
 }
