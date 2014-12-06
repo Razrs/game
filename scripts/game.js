a7afc97a9273d1fdbbf56a3caecce6e7163b19ba
Makanju Dami Perfect <damiperfect@gmail.com>
2014-12-06 21:00:01 +0000

    Final landing page commit

--- scripts/game.js
+++ scripts/game.js
@@ -3,11 +3,15 @@ var stage = new PIXI.Stage(0x000000);
 var renderer = new PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
 
 // allow renderer to fill screen
-// renderer.view.style.display = "block";
+renderer.view.style.display = "block";
 // renderer.view.style.width = "100%"
 // renderer.view.style.height = "100%"
 
 var square;
+var endsquare;
+var bottomleftsquare;
+var bottomrightsquare;
+
 
 
 function load() {
@@ -28,14 +32,24 @@ function load() {
     
     
     //postion start button at center
-    
+    startButtonSprite.anchor.x = 0.5;
+    startButtonSprite.anchor.y = 0.5;
+    startButtonSprite.position.x = window.innerWidth/2;
+    startButtonSprite.position.y = window.innerHeight/2;
     
     //add the sprites to stage;
     stage.addChild(homeBackgroundSprite);
     stage.addChild(startButtonSprite);
     
+	endsquare = endSquare();
 	square = newSquare();
+	bottomleftsquare = bottomLeftSquare();
+	bottomrightsquare = bottomRightSquare();
+	
 	stage.addChild(square);
+	stage.addChild(endsquare);
+	stage.addChild(bottomrightsquare);
+	stage.addChild(bottomleftsquare);
 	
     
     
@@ -46,8 +60,11 @@ function load() {
 function animate() {
 	// Recursion to keep rendering
 	setTimeout(animate, 1000 / 60);
-
+	
 	square.rotation += 0.1;
+	endsquare.rotation +=0.1;
+	bottomleftsquare.rotation += 0.1;
+	bottomrightsquare.rotation += 0.1;
 
 	// Render to the stage
 	renderer.render(stage);
@@ -64,3 +81,55 @@ function newSquare() {
 
 	return square;
 }
+
+
+function newSquare() {
+	var square = new PIXI.Sprite(textureSquare);
+	// anchor is % not pixels, so 0.5 is center
+	square.anchor.x = 0.5;
+	square.anchor.y = 0.5;
+
+	square.position.x = 0;
+	square.position.y = 0;
+
+	return square;
+}
+
+function endSquare(){
+    var squareend = new PIXI.Sprite(textureSquare);
+    squareend.anchor.x = 0.5;
+    squareend.anchor.y = 0.5;
+    
+    squareend.position.x = window.innerWidth - 3;
+    squareend.position.y = 0;
+    
+    return squareend;
+    
+}
+
+function bottomLeftSquare(){
+    var btmleftsquare = new PIXI.Sprite(textureSquare);
+    btmleftsquare.anchor.x = 0.5;
+    btmleftsquare.anchor.y = 0.5;
+    
+    btmleftsquare.position.x = 0;
+    btmleftsquare.position.y = window.innerHeight -3;
+    
+    return btmleftsquare;
+}
+
+function bottomRightSquare(){
+    var btmrightsquare = new PIXI.Sprite(textureSquare);
+    btmrightsquare.anchor.x = 0.5;
+    btmrightsquare.anchor.y = 0.5;
+    
+    btmrightsquare.position.x = window.innerWidth -3;
+    btmrightsquare.position.y = window.innerHeight -3;
+    
+    return btmrightsquare;
+}
+
+
+
+
+
