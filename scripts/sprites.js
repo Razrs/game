ede3d5441c7d8d83c057636b1036b20520ea48aa
Benjamin Cheng <lolzballs.mc@gmail.com>
2014-12-06 15:32:21 -0500

    Made background image size of the renderer

--- scripts/sprites.js
+++ scripts/sprites.js
@@ -4,7 +4,9 @@ var startButtonSprite;
 
 function initSprites(){
     //Load background for Home
-    homeBackgroundSprite =   PIXI.Sprite.fromImage("assets/textures/start_background.png");  
+    homeBackgroundSprite = PIXI.Sprite.fromImage("assets/textures/start_background.png");
+    homeBackgroundSprite.width = renderer.width;
+    homeBackgroundSprite.height = renderer.height;
     
     startButtonSprite = PIXI.Sprite.fromImage("assets/textures/start_button.png");
 }
