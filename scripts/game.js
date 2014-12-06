1fe75ff3749e3361ad4ff41d831913836681cfcf
Benjamin Cheng <lolzballs.mc@gmail.com>
2014-12-06 16:38:00 -0500

    Started seperating menu and game

--- scripts/game.js
+++ scripts/game.js
@@ -12,47 +12,53 @@ var endsquare;
 var bottomleftsquare;
 var bottomrightsquare;
 
-
-
-function load() {
-	// Add the element to body
-	document.body.appendChild(renderer.view);
-
-    //initialize textures
-	initTextures();
-	
-	//intialize Sprites
-	initSprites();
-
+function loadMenu() {
     //postion background at center
     homeBackgroundSprite.anchor.x = 0.5;
     homeBackgroundSprite.anchor.y = 0.5;
     homeBackgroundSprite.position.x = window.innerWidth/2;
     homeBackgroundSprite.position.y = window.innerHeight/2;
-    
-    
+
+
     //postion start button at center
     startButtonSprite.anchor.x = 0.5;
     startButtonSprite.anchor.y = 0.5;
     startButtonSprite.position.x = window.innerWidth/2;
     startButtonSprite.position.y = window.innerHeight/2;
-    
+
     //add the sprites to stage;
     stage.addChild(homeBackgroundSprite);
     stage.addChild(startButtonSprite);
-    
-	endsquare = endSquare();
-	square = newSquare();
-	bottomleftsquare = bottomLeftSquare();
-	bottomrightsquare = bottomRightSquare();
-	
+
+	endsquare = newSquare(window.innerWidth - 3, 0);
+	square = newSquare(0, 0);
+	bottomleftsquare = newSquare(0, window.innerHeight - 3);
+	bottomrightsquare = newSquare(window.innerWidth - 3, window.innerHeight - 3);
+
 	stage.addChild(square);
 	stage.addChild(endsquare);
 	stage.addChild(bottomrightsquare);
 	stage.addChild(bottomleftsquare);
+}
+
+function loadGame() {
 	
-    
-    
+}
+
+function load() {
+	// Add the element to body
+    //initialize textures
+	initTextures();
+
+	//intialize Sprites
+	initSprites();
+
+	loadMenu();
+
+    // Removes the "loading..." text
+    document.getElementById("loading").remove();
+    document.body.appendChild(renderer.view);
+
 	// Gets an anim frame, equalviant to render method
 	setTimeout(animate, 1000 / 60);
 }
@@ -60,7 +66,7 @@ function load() {
 function animate() {
 	// Recursion to keep rendering
 	setTimeout(animate, 1000 / 60);
-	
+
 	square.rotation += 0.1;
 	endsquare.rotation +=0.1;
 	bottomleftsquare.rotation += 0.1;
@@ -70,66 +76,14 @@ function animate() {
 	renderer.render(stage);
 }
 
-function newSquare() {
+function newSquare(x, y) {
 	var square = new PIXI.Sprite(textureSquare);
 	// anchor is % not pixels, so 0.5 is center
 	square.anchor.x = 0.5;
 	square.anchor.y = 0.5;
 
-	square.position.x = 0;
-	square.position.y = 0;
+	square.position.x = x;
+	square.position.y = y;
 
 	return square;
 }
-
-
-function newSquare() {
-	var square = new PIXI.Sprite(textureSquare);
-	// anchor is % not pixels, so 0.5 is center
-	square.anchor.x = 0.5;
-	square.anchor.y = 0.5;
-
-	square.position.x = 0;
-	square.position.y = 0;
-
-	return square;
-}
-
-function endSquare(){
-    var squareend = new PIXI.Sprite(textureSquare);
-    squareend.anchor.x = 0.5;
-    squareend.anchor.y = 0.5;
-    
-    squareend.position.x = window.innerWidth - 3;
-    squareend.position.y = 0;
-    
-    return squareend;
-    
-}
-
-function bottomLeftSquare(){
-    var btmleftsquare = new PIXI.Sprite(textureSquare);
-    btmleftsquare.anchor.x = 0.5;
-    btmleftsquare.anchor.y = 0.5;
-    
-    btmleftsquare.position.x = 0;
-    btmleftsquare.position.y = window.innerHeight -3;
-    
-    return btmleftsquare;
-}
-
-function bottomRightSquare(){
-    var btmrightsquare = new PIXI.Sprite(textureSquare);
-    btmrightsquare.anchor.x = 0.5;
-    btmrightsquare.anchor.y = 0.5;
-    
-    btmrightsquare.position.x = window.innerWidth -3;
-    btmrightsquare.position.y = window.innerHeight -3;
-    
-    return btmrightsquare;
-}
-
-
-
-
-
